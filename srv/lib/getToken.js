fd241957f31158f9530f99fd3f3c065342a97ee7
Hao Nguyen <haonguyen@abeam.com>
2023-09-06 09:43:54 +0000

    Create service for Supplier

---
+++ srv/lib/getToken.js
@@ -0,0 +1,19 @@
+const cds = require("@sap/cds");
+const oConnect = require('@sap-cloud-sdk/connectivity')
+
+async function getToken(){
+
+    const oVisaDesInf = await oConnect.getDestination({destinationName: "VISAProcesses-srv" })
+    console.log(oVisaDesInf)
+    let vAuthType  = oVisaDesInf.authTokens[0].type;
+    let vAuthToken = oVisaDesInf.authTokens[0].value;
+    let oResult = {
+        "token" :  vAuthType + " " + vAuthToken,
+        "url": oVisaDesInf.url
+    }
+
+    return oResult;
+
+}
+
+module.exports = { getToken };
